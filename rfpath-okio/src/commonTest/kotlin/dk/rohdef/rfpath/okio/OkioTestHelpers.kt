04672c942298d2f98a98c7485a620b8cac090593
Rohde Fischer <rohdef@rohdef.dk>
2023-04-25 22:13:48 +0200

    Experiements with builder

--- rfpath-okio/src/commonTest/kotlin/dk/rohdef/rfpath/okio/OkioTestHelpers.kt
+++ rfpath-okio/src/commonTest/kotlin/dk/rohdef/rfpath/okio/OkioTestHelpers.kt
@@ -1,11 +1,58 @@
 package dk.rohdef.rfpath.okio
 
+import arrow.core.Either
+import arrow.core.continuations.either
+import arrow.core.getOrHandle
+import dk.rohdef.rfpath.DirectoryError
+import dk.rohdef.rfpath.MakeDirectoryError
+import dk.rohdef.rfpath.MakeFileError
+import dk.rohdef.rfpath.test.TestDirectory
+import dk.rohdef.rfpath.test.TestFile
+import dk.rohdef.rfpath.test.root
 import okio.FileSystem
 import okio.Path
 import okio.Path.Companion.toPath
 import okio.fakefilesystem.FakeFileSystem
 
 class OkioTestHelpers {
+    class OkioTestDirectory(
+        private val directory: OkioDirectory,
+        path: List<String>,
+    ) : TestDirectory<OkioTestDirectory>(path), dk.rohdef.rfpath.Path.Directory by directory {
+        override suspend fun makeDirectory(directoryName: String): Either<MakeDirectoryError, OkioTestDirectory> =
+            either {
+                val newDirectory = directory.makeDirectory(directoryName)
+                    .mapLeft { TODO("Look into the error cases and what to do") }
+                    .bind() as OkioDirectory
+
+                OkioTestDirectory(newDirectory, path + directoryName)
+            }
+
+        override suspend fun list(): Either<DirectoryError, List<dk.rohdef.rfpath.Path<*, *>>> {
+            return directory.list()
+                .mapLeft { TODO("Look into the error cases and what to do") }
+        }
+
+        override suspend fun makeFile(fileName: String): Either<MakeFileError, TestFile<*>> {
+            TODO("Handle creating proper OkioTestFiles")
+        }
+    }
+
+    private suspend fun x() {
+        root({
+            directory("poc") {}
+        }) {
+            // TODO yet another nasty cast
+            OkioTestDirectory(
+                OkioDirectory.directory(fileSystem(), "/".toPath())
+                    .getOrHandle { TODO() }
+                        as OkioDirectory,
+                it,
+            )
+        }
+    }
+
+
     private val root = "/".toPath()
     val basePath = root.resolve("test")
 
@@ -62,12 +109,14 @@ class OkioTestHelpers {
             writeUtf8("barbaz")
         }
         fileSystem.write(path.resolve(dummyFilename3), true) {
-            writeUtf8("""
+            writeUtf8(
+                """
                 Multiple
                 Lines
                 In
                 File
-            """.trimIndent())
+            """.trimIndent()
+            )
         }
     }
 }
--- rfpath-test/src/commonMain/kotlin/dk/rohdef/rfpath/test/TestFileSystemFactory.kt
+++ rfpath-test/src/commonMain/kotlin/dk/rohdef/rfpath/test/TestFileSystemFactory.kt
@@ -1,23 +1,31 @@
 package dk.rohdef.rfpath.test
 
 import arrow.core.getOrHandle
-import dk.rohdef.rfpath.Path
 import dk.rohdef.rfpath.permissions.Permission
 import dk.rohdef.rfpath.permissions.Permissions
 
-suspend fun root(configure: DirectoryContext.() -> Unit): Path.Directory {
-    val builder: suspend (path: List<String>) -> TestDirectory = { TestDirectoryDefault.createUnsafe(it) }
+suspend fun root(
+    configure: DirectoryContext<TestDirectoryDefault>.() -> Unit,
+): TestDirectoryDefault {
+    val builder: suspend (path: List<String>) -> TestDirectoryDefault = { TestDirectoryDefault.createUnsafe(it) }
+    return root(configure, builder)
+}
+
+suspend fun <D : TestDirectory<D>> root(
+    configure: DirectoryContext<D>.() -> Unit,
+    builder: suspend (path: List<String>) -> D,
+): D {
     val rootDirectory = DirectoryContext(emptyList(), builder)
     rootDirectory.configure()
 
     return rootDirectory.build()
 }
 
-class DirectoryContext(
+class DirectoryContext<D : TestDirectory<D>>(
     val path: List<String>,
-    var builder: suspend (path: List<String>) -> TestDirectory,
+    val builder: suspend (path: List<String>) -> D,
 ) {
-    val directories = mutableMapOf<String, DirectoryContext>()
+    val directories = mutableMapOf<String, DirectoryContext<D>>()
     val files = mutableMapOf<String, FileContext>()
 
     var permissions = Permissions(
@@ -26,37 +34,37 @@ class DirectoryContext(
         setOf(Permission.READ, Permission.EXECUTE),
     )
 
-    fun directory(directoryName: String, configure: DirectoryContext.() -> Unit) {
+    fun directory(directoryName: String, configure: DirectoryContext<D>.() -> Unit) {
         val directory = DirectoryContext(path + directoryName, builder)
         directory.configure()
 
-        directories.put(directoryName, directory)
+        directories[directoryName] = directory
     }
 
     fun file(fileName: String, configure: FileContext.() -> Unit) {
         val file = FileContext(path + fileName)
         file.configure()
 
-        files.put(fileName, file)
+        files[fileName] = file
     }
 
-    suspend fun build(): TestDirectory {
+    suspend fun build(): D {
         return build(builder)
     }
 
-    private suspend fun build(builder: suspend (path: List<String>)->TestDirectory): TestDirectory {
+    private suspend fun build(builder: suspend (path: List<String>) -> D): D {
         val directory = builder(path)
 
         directories.forEach { subDirectory ->
             subDirectory.value.build {
                 directory.makeDirectory(subDirectory.key)
-                    .getOrHandle { throw RuntimeException("This is not possible in the test structure: ${it}") }
+                    .getOrHandle { throw RuntimeException("This is not possible in the test structure: $it") }
             }
         }
         files.forEach { subFile ->
             subFile.value.build {
                 directory.makeFile(subFile.key)
-                    .getOrHandle { throw RuntimeException("This is not possible in the test structure: ${it}") }
+                    .getOrHandle { throw RuntimeException("This is not possible in the test structure: $it") }
             }
         }
 
@@ -75,7 +83,7 @@ class FileContext(
         setOf(Permission.READ),
     )
 
-    internal suspend fun build(builder: suspend (path: List<String>)->TestFile<*>): TestFile<*> {
+    internal suspend fun build(builder: suspend (path: List<String>) -> TestFile<*>): TestFile<*> {
         val file = builder(path)
 
         file.contents = contents
